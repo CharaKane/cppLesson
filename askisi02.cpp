8d6d77fb015dede55b9bc862abed68cbd4748cc4
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-03-30 12:43:05 +0300

    dose 2 arithmous

---
+++ askisi02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+ int x, y;
+
+ cout << "dose duo arithmous:\n" << endl;
+
+ cin >> x >> y;
+
+  cout << "To athrisma ton arithmon einai:" << (x+y) << endl;
+  cout << "To ginomeno ton arithmon einai:" << (x*y) << endl;
+
+return 0;
+
+}
