a0e8bb0bb84b05a0b43dd84072e7ff4de3763e96
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-03-30 13:02:34 +0300

    a++ / ++a

---
+++ askisi10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, c, d;
+      a=b=0;
+      d=c=0;
+      a=b++;
+      c=++d;
+
+      cout << "a=" << a << endl << "b=" << b << endl;
+      cout << "c=" << c << endl << "d=" << d << endl;
+      
+}
