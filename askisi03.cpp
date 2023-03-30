00a0cdbf649b0fbf6fe5112304deab5a8138a2c9
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-03-30 12:48:02 +0300

    ogkosSfairas

---
+++ askisi03.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#define pi 3.14
+using namespace std;
+
+int main(){
+  float r;
+ cout << "Dvse aktina sfairas:";
+ cin >> r;
+
+ cout << "O ogkos tiw sfairas me aktina: \n" << (r) <<"\n einai:\n" << (4*pi*r*r*r/3) <<endl;
+
+ return 0;
+}
