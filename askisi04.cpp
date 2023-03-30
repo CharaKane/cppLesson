57d9ff2e1a998e155908b40d5b58ff30b64fd66d
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-03-30 12:50:24 +0300

    arnitikos thetikos

---
+++ askisi04.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+ using namespace std;
+ 
+int main(){
+  double number;
+
+  cout << "dose enan arithmo:\n" << endl;
+  cin >> number;
+
+if(number<0.0) 
+ 
+ cout << "dosate arnitiko arithmo" << endl;
+
+ else if (number>0.0)
+
+ cout << "dosate thetiko arithmo" << endl;
+
+ else 
+
+ cout << "dosate 0" << endl;
+
+ return 0;
+}
