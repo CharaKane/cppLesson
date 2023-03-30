f906879fd44d6218d94ff1158e121f4d56724d3a
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-03-30 12:51:41 +0300

    dose vathmo

---
+++ askisi05.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int v;
+
+cout << "dose vathmo:\n" <<endl;
+cin >> v;
+
+  if(v>100)
+   cout << "akuro" <<endl;
+
+  else if (v>=90)
+
+ cout << "Excellent" << endl;
+
+  else if (v>=80)
+  
+ cout << "Very Good" << endl;
+
+   else if (v>=60)
+   
+ cout << "Good" << endl;
+
+  else if (v>=50)
+
+ cout << "Average" << endl;
+
+     else if (v>=40)
+	 
+ cout << "Poor" << endl;
+
+    else if (v>=0)
+     cout << "Not promoted" << endl;
+  
+
+   else 
+   cout << "arnhtikos" << endl;
+
+     return 0;
+
+}
