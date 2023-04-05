8b72cdd9f559ab3edd232ab6439aec89e2223b3e
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-04-05 15:56:03 +0300

    dose times ston pinaka

---
+++ askisi20.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int A[5],i;
+	
+	cout << "Dose 5 arithmous:\n" << endl;
+	
+	for(i=0; i<5; i++){
+		cin >> A[i];
+	}
+	
+	cout <<"O pinakas einai o:\n" << endl;
+	
+		for(i=0; i<5; i++){
+		cout << A[i] ;
+		
+	}
+ return 0;
+}
